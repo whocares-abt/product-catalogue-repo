9f15496da28bedc3b395a2ef65870f3705b47215
whocares-abt <126311601+whocares-abt@users.noreply.github.com>
2025-03-16 18:25:53 +0530

    Trying alternate version

--- src/scripts.js
+++ src/scripts.js
@@ -6,27 +6,34 @@ function displayData(data) {
     const dataContainer = document.getElementById('data-container');
     
     dataContainer.innerHTML = '';
-    console.log(typeof(data))
-    data = Object.entries(data)
+    data = Object.entries(data);
     // Iterate over the data and create HTML elements to display it
-    data.forEach(
-        item => {
+    console.log(data);
+
+    data.forEach(item => {
         const dataItem = document.createElement('div');
         dataItem.classList.add('data-item');
         dataItem.textContent = `ProductId: ${item.ProductId}, ProductName: ${item.ProductName}, Price (in Dollars): ${item.Price}, Description: ${item.Description}`;
         dataContainer.appendChild(dataItem);
-    }
-    );
+    });
 }
 
 
 async function list() {
     const endpoint = '/data-api/rest/Person';
+    fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+        displayData(data);
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
     // try {
-    const response = await fetch(endpoint);
-    const data = await response.json();
-    displayData(data)
-    console.table(data.value);
+    // const response = await fetch(endpoint);
+    // const data = await response.json();
+    // displayData(data)
+    // console.table(data.value);
     // }
     // catch (error) {
     //     console.log("Error Fetching Data. Please try again after some time. If error persists, try contacting the website owner.")
@@ -38,7 +45,7 @@ function trial(){
 }
 
 async function add() {
-    console.log("Hi 6");
+    console.log("Hello 1");
     // const data = {
     //   Name: "Pedro"
     // };
