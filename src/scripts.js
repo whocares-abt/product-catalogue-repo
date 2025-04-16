25bb32a77c9485df55497a4451cae074f60068c6
whocares-abt <126311601+whocares-abt@users.noreply.github.com>
2025-04-16 23:24:41 +0530

    Insights

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,12 @@
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+const appInsights = new ApplicationInsights({ config: {
+  connectionString: "InstrumentationKey=52087622-6785-4334-9341-8d16129d6719;IngestionEndpoint=https://eastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://eastasia.livediagnostics.monitor.azure.com/;ApplicationId=c77d492f-c96e-4e7d-a800-e7ae7d11250d"
+  /* ...Other Configuration Options... */
+} });
+appInsights.loadAppInsights();
+appInsights.trackPageView();
+
 document.addEventListener('DOMContentLoaded', function() {
     list();
 });
